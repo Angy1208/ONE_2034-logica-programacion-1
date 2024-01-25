031c152bd4efdfe62aa01f72ee320bbe441cb744
Angy1208 <ang.floresv@gmail.com>
2024-01-25 16:20:01 -0600

    Avance de numero secreto

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 alert('Hola Mundo');
 let numeroSecreto = 6;
 let numeroUsuario = 0;
-   
+//Contador
+let intentos = 1;
+let palabraVeces = 'vez';   
 // Loops y Bucles
 while (numeroUsuario != numeroSecreto) { 
     let numeroUsuario = prompt('Me indicas un numero porfavor del 1 al 10');
@@ -13,12 +15,20 @@ while (numeroUsuario != numeroSecreto) {
 
     //Condicionales anidados
     if (numeroUsuario == numeroSecreto) {
-        alert('Acertaste, el numero es: ' + numeroSecreto);
+        alert('Acertaste, el numero es: ' + numeroSecreto + " Lo hiciste en: " + intentos + palabraVeces);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El numero secreto es menor');
         } else {
             alert('El numero secreto es mayor');
+        }
+        // El contador se incrementa cuando no acierta y ajuste de vez -> veces
+        intentos = intentos + 1;
+        palabraVeces = 'veces';
+        //break romper forzadamente el programa
+        if (intentos > 3) { 
+        alert('Llegaste al maximo de intentos');
+        break;
         }
     }
 }
